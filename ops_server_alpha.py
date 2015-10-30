6457dd3a34fad642216fb2f862f8e556f8ecfa23
berton.liu <bostoneboy@gmail.com>
2015-10-30 15:39:36 +0800

    add custom port support for server running

--- ops_server_alpha.py
+++ ops_server_alpha.py
@@ -15,10 +15,10 @@ import time
 import os
 
 import salt.client
+import ConfigParser
 from salt.client.ssh.client import SSHClient
 from logd import logger
 from url import url
-
 
 settings = \
     {
@@ -41,10 +41,14 @@ if __name__ == "__main__":
 #启动tornado，配置里如果打开debug，则可以使用autoload，属于development模式
 #如果关闭debug，则不可以使用autoload，属于production模式
 #autoload的含义是当tornado监测到有任何文件发生变化，不需要重启server即可看到相应的页Ã§¹浠裨蚴切薷牧硕骺床坏奖浠??
-    logger.info('Server start.')
+
+    try:
+        port = sys.argv[1]  
+    except:
+        port = 83    #default server port
+    logger.info('Server start at port %s ',port)
 
     server = tornado.httpserver.HTTPServer(application)
-    server.bind(8890) 		#绑定到10002端口
-    server.start() 		#自动以多进程方式启动Tornado，否则需要手工启动多个进程
-    #server.start(0) 		#自动以多进程方式启动Tornado，否则需要手工启动多个进程
+    server.bind(port)              
+    server.start() 
     tornado.ioloop.IOLoop.instance().start()
